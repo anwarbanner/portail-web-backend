27f8e4ede7273e65bf7cfba85f8680aed6e36b9c
anwarbanner <laklatyanwar@gmail.com>
2026-05-13 23:47:25 +0100

    initialisation

---
+++ src/main/java/portail/web/backend/exemple/portail/web/backend/PortailWebBackendApplication.java
@@ -0,0 +1,13 @@
+package portail.web.backend.exemple.portail.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portail/web/backend/exemple/portail/web/backend/PortailWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package portail.web.backend.exemple.portail.web.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
